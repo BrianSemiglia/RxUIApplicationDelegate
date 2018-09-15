85300417d8b767c9e3965c9d1629e733d5a91f1c
Brian Semiglia <brian.semiglia@gmail.com>
2018-09-14 23:32:04 -0400

    Adds example.

---
+++ RxUIApplicationDelegateExample/AppDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  AppDelegate.swift
+//  RxUIApplicationDelegateExample
+//
+//  Created by brian.semiglia@gmail.com on 09/12/2018.
+//  Copyright (c) 2018 brian.semiglia@gmail.com. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxUIApplicationDelegate
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  let backing = RxUIApplicationDelegate(
+    initial: .empty
+  )
+  let cleanup = DisposeBag()
+  
+  override init() {
+    backing.render(
+      new: .empty,
+      old: .empty
+    )
+    backing
+      .output
+      .subscribe { _ in }
+      .disposed(
+        by: cleanup
+      )
+  }
+  
+  override open func forwardingTarget(for input: Selector!) -> Any? { return
+    backing
+  }
+  
+  override open func responds(to input: Selector!) -> Bool { return
+    backing
+      .responds(
+        to: input
+      )
+  }
+
+}
+
---
+++ RxUIApplicationDelegateExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxUIApplicationDelegateExample
+//
+//  Created by Brian Semiglia on 9/14/18.
+//  Copyright © 2018 Brian Semiglia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
