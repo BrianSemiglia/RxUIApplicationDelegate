3fcd9a5988a97f93b32b551a953b5fca5220b546
Brian Semiglia <brian.semiglia@gmail.com>
2018-09-14 23:11:46 -0400

    Adds project files.

---
+++ RxUIApplicationDelegate/RxUIApplicationDelegate.h
@@ -0,0 +1,19 @@
+//
+//  RxUIApplicationDelegate.h
+//  RxUIApplicationDelegate
+//
+//  Created by Brian Semiglia on 9/14/18.
+//  Copyright © 2018 Brian Semiglia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxUIApplicationDelegate.
+FOUNDATION_EXPORT double RxUIApplicationDelegateVersionNumber;
+
+//! Project version string for RxUIApplicationDelegate.
+FOUNDATION_EXPORT const unsigned char RxUIApplicationDelegateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxUIApplicationDelegate/PublicHeader.h>
+
+
